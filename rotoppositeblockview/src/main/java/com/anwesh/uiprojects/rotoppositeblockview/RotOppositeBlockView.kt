7abdf9f5453c373261035e061dde6a06769f1e8b
Anwesh43 <anweshthecool0@gmail.com>
2020-09-19 03:18:56 +0530

    adding constants and libraries

--- rotoppositeblockview/src/main/java/com/anwesh/uiprojects/rotoppositeblockview/RotOppositeBlockView.kt
+++ rotoppositeblockview/src/main/java/com/anwesh/uiprojects/rotoppositeblockview/RotOppositeBlockView.kt
@@ -4,3 +4,26 @@ package com.anwesh.uiprojects.rotoppositeblockview
  * Created by anweshmishra on 19/09/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#009688",
+        "#3F51B5",
+        "#4CAF50",
+        "#03A9F4"
+).map({Color.parseColor(it)}).toTypedArray()
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+
