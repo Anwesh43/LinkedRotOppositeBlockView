fcc56e93d6f72410bdaa2481b06bf8014d67bd05
Anwesh43 <anweshthecool0@gmail.com>
2020-09-19 04:00:04 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedrotoppositeblockview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrotoppositeblockview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedrotoppositeblockview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.rotoppositeblockview.RotOppositeBlockView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotOppositeBlockView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
